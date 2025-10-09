79423d8467f569a2867e93b8c72a67b57a7d3b88
Christopher-Guarino1 <christopher.guarino1@ibm.com>
2025-10-09 14:57:14 -0400

    Issue with repeated numbers in list

---
+++ sum_pair.py
@@ -0,0 +1,21 @@
+def pair_sum(numlist, k): 
+    solutions = []
+    for i in numlist:
+        target  = k - i 
+        if target in numlist: 
+            solutions.append([i,target])
+            numlist.remove(i)
+            numlist.remove(target)
+            print(f"i: {numlist}, removed {i} and {target}")
+
+    
+    if solutions == []: 
+        print("No sum pairs found for k in numlist.")
+    else: print(f"sum pairs in numlist: {solutions}")
+    print("numlist:",numlist)
+
+def main(): 
+    return pair_sum([6,1,3,2,2,2,2,3,1,4,0], 4)
+
+if __name__ == "__main__":
+    main()
